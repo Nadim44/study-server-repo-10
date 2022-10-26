07ad46e3bbc02764bac584f38fa416d02d4ac948
2Nadim <nadimmahamud661@gmail.com>
2022-10-26 15:33:01 +0100

    course added

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = 5000;
 
-const courses = require('./data/courses.json')
+app.use(cors());
 
+const courses = require('./data/courses.json');
+const details = require('./dataFile/details.json')
 
 app.get('/', (req, res) => {
     res.send('course API running');
@@ -11,6 +14,31 @@ app.get('/', (req, res) => {
 
 app.get('/course-categories', (req, res) => {
     res.send(courses)
+})
+
+app.get('/course', (req, res) => {
+    res.send(details);
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (id === '07') {
+        res.send(details);
+    }
+    else {
+        const category_course = details.filter(c => c.category_id == id);
+        res.send(category_course);
+
+    }
+
+})
+
+app.get('/details/:id', (req, res) => {
+    const id = req.params.id;
+    // console.log(req.params.id)
+    const selectedCourse = details.find(c => c._id === id);
+    res.send(selectedCourse)
 })
 
 app.listen(port, () => {
