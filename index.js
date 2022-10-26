212f3d3643d675427d931f15b704bc381ea3aa07
2Nadim <nadimmahamud661@gmail.com>
2022-10-26 07:19:17 +0100

    data loaded

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express();
 const port = 5000;
 
+const courses = require('./data/courses.json')
+
+
 app.get('/', (req, res) => {
     res.send('course API running');
+})
+
+app.get('/course-categories', (req, res) => {
+    res.send(courses)
 })
 
 app.listen(port, () => {
